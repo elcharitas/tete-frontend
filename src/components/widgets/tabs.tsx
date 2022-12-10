91532510e8015f3d81d76fe4d0548ccf9ad75d10
elcharitas <jonathanirhodia@gmail.com>
2022-12-10 03:14:54 -0500

    chore: fix colors use

--- src/components/widgets/tabs.tsx
+++ src/components/widgets/tabs.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { TabContext, TabList, TabPanel } from "@mui/lab";
 import { Box, Tab, Divider, Typography } from "@mui/material";
 import { bets } from "/src/api/data/bet-history";
-import { teteBlue, teteGray, teteRed, teteWhite } from "/src/api/providers/theme/colors";
 
 export default function LabTabs() {
     const [value, setValue] = useState("1");
@@ -59,8 +58,10 @@ export default function LabTabs() {
                                         color: "common.white",
                                         width: "100px",
                                         margin: " auto 0",
-                                        bgcolor: `${bet.status === "Lost" ? (teteRed) : (teteBlue)}`,
-                                        background:`${bet.status === "Running" ? (teteGray) : (null)}`
+                                        bgcolor:
+                                            bet.status === "Running"
+                                                ? "secondary.main"
+                                                : "error.main",
                                     }}
                                 >
                                     {bet.status}
@@ -72,11 +73,16 @@ export default function LabTabs() {
                 </TabPanel>
                 <TabPanel value="2">No Bets Available</TabPanel>
                 <TabPanel value="3">
-                    {bets.map(bet =>{
-                        if(bet.status === "Running")
-                            return(
+                    {bets.map((bet) => {
+                        if (bet.status === "Running") {
+                            return (
                                 <>
-                                    <Box sx={{display:"flex", justifyContent:"space-between"}}>
+                                    <Box
+                                        sx={{
+                                            display: "flex",
+                                            justifyContent: "space-between",
+                                        }}
+                                    >
                                         <div>
                                             <Typography>Date</Typography>
                                             <Typography>{bet.date}</Typography>
@@ -90,17 +96,21 @@ export default function LabTabs() {
                                             <Typography>{bet.stake}</Typography>
                                         </div>
                                         <div>
-                                            <Typography>Potential Winnings</Typography>
-                                            <Typography>{bet.potentialWinnings}</Typography>
+                                            <Typography>
+                                                Potential Winnings
+                                            </Typography>
+                                            <Typography>
+                                                {bet.potentialWinnings}
+                                            </Typography>
                                         </div>
                                         <Typography
                                             sx={{
-                                                border:"2px solid",
-                                                textAlign:"center",
-                                                color:teteWhite,
-                                                width:"100px",
-                                                margin:" auto 0",
-                                                background:teteGray
+                                                border: "2px solid",
+                                                textAlign: "center",
+                                                color: "common.white",
+                                                width: "100px",
+                                                margin: " auto 0",
+                                                background: "secondary.main",
                                             }}
                                         >
                                             {bet.status}
@@ -108,9 +118,10 @@ export default function LabTabs() {
                                     </Box>
                                     <Divider />
                                 </>
-                            )
+                            );
+                        }
+                        return null;
                     })}
-
                 </TabPanel>
             </TabContext>
         </Box>
